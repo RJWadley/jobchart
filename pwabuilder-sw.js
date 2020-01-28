5f03477abde0478d8c86892d38545d0480dd276c
Robbie <robbiethewadley@gmail.com>
2020-01-28 22:15:08 +0000

    icon caching

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,7 +3,14 @@
 const CACHE = "pwabuilder-precache";
 const precacheFiles = [
   /* Add an array of files to precache for your app */
-  "./images/icons/*" //all pwa icons
+  "./images/icons/icon-72x72.png",
+  "./images/icons/icon-96x96.png",
+  "./images/icons/icon-128x128.png",
+  "./images/icons/icon-144x144.png",
+  "./images/icons/icon-152x152.png",
+  "./images/icons/icon-192x192.png",
+  "./images/icons/icon-384x384.png",
+  "./images/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", function (event) {
