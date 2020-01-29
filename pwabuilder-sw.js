c0a4b63323b25fc21b38faf0b2eef890579c4d18
Robbie <robbiethewadley@gmail.com>
2020-01-29 05:30:04 +0000

    fix error

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,14 +3,6 @@
 const CACHE = "pwabuilder-precache";
 const precacheFiles = [
   /* Add an array of files to precache for your app */
-  "./images/icons/icon-72x72.png",
-  "./images/icons/icon-96x96.png",
-  "./images/icons/icon-128x128.png",
-  "./images/icons/icon-144x144.png",
-  "./images/icons/icon-152x152.png",
-  "./images/icons/icon-192x192.png",
-  "./images/icons/icon-384x384.png",
-  "./images/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", function (event) {
